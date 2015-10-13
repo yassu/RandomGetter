434c160c23965cec4361a86d6219f21d23cc7d1a
yassu <mathyassu@gmail.com>
2015-10-13 12:44:50 +0900

    IntRandomType

--- src/random_getter.py
+++ src/random_getter.py
@@ -7,6 +7,47 @@ DEFAULT_RANDOM_LENGTH = 4
 DEFAULT_RANDOM_NUMBER = 1
 DEFAULT_LENGTH_TYPE = 0 # means not absolute
 
+class RandomType(object):
+    DEFAULT_LENGTH = None
+
+    def __init__(self, length=DEFAULT_RANDOM_LENGTH, min_value=None, max_value=None):
+        self._length = length
+        self._min_value = min_value
+        self._max_value = max_value
+
+    @property
+    def length(self):
+        return self._length
+
+    @property
+    def min_value(self):
+        return self._min_value
+
+    @property
+    def max_value(self):
+        return self._max_value
+
+    def get_random(self):
+        pass
+
+class IntRandomType(RandomType):
+    DEFAULT_LENGTH = 4
+
+    def __init__(self, length=DEFAULT_RANDOM_LENGTH, min_value=None,
+            max_value=None):
+        RandomType.__init__(self, length=length, min_value=min_value,
+        max_value=max_value)
+
+    def get_random(self):
+        # consider min_value, max_value and length
+        # (TODO: case absolute length type)
+        min_value = -(10**(self.length + 1) - 1)
+        if self.max_value is None:
+            max_value = 10**(self.length + 1) - 1
+        else:
+            max_value = min(self.max_value, 10**(self.length + 1) - 1)
+        return random.randint(min_value, max_value)
+
 if __name__ == '__main__':
     defalt_random_type = int
     random_number = DEFAULT_RANDOM_NUMBER
--- test/random_getter_test.py
+++ test/random_getter_test.py
@@ -1,1 +1,38 @@
 # coding: UTF-8
+
+from sys import path
+path.append('src')
+from random_getter import *
+
+from unittest import TestCase
+
+class RandomTypeTestCase(TestCase):
+    def length_test(self):
+        r = RandomType()
+        print(r.length)
+        assert(r.length == DEFAULT_RANDOM_LENGTH)
+
+    def length_test2(self):
+        r = RandomType(length=10)
+        assert(r.length == 10)
+
+    def min_value_test(self):
+        r = RandomType()
+        assert(r.min_value is None)
+
+    def min_value_test2(self):
+        r = RandomType(min_value=100)
+        assert(r.min_value == 100)
+
+    def max_value_test(self):
+        r = RandomType()
+        assert(r.max_value is None)
+
+    def max_value_test2(self):
+        r = RandomType(max_value=100)
+        assert(r.max_value == 100)
+
+class IntRandomTestCase(TestCase):
+    def get_random_test(self):
+        r = IntRandomType()
+        assert(- 10**5 < r.get_random() < 10**5)
