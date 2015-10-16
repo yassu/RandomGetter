f4bbceb4d263fb03d90b7cae4799b7a032b9251d
yassu <mathyassu@gmail.com>
2015-10-17 02:11:48 +0900

    more strict tests for random value

--- test/random_getter_test.py
+++ test/random_getter_test.py
@@ -4,6 +4,8 @@ from random_getter.random_getter import *
 import unittest
 import re
 
+RANDOM_TEST_NUMBER = 10
+
 def get_default_option():
     options, _ = get_parser().parse_args()
     return options
@@ -11,16 +13,17 @@ def get_default_option():
 def get_random_from_format_test():
     options = get_default_option()
     options.fo = "[int]"
-    res = get_random_from_format(options)
-    assert(re.search(r'(-)?\d+', res))
-    res = int(res)
-    assert(-10**(DEFAULT_RANDOM_LENGTH + 1) <
-           res < 10**(DEFAULT_RANDOM_LENGTH + 1))
+    for _ in range(RANDOM_TEST_NUMBER):
+        res = get_random_from_format(options)
+        assert(re.search(r'(-)?\d+', res))
+        res = int(res)
+        assert(-10**(DEFAULT_RANDOM_LENGTH + 1) <
+               res < 10**(DEFAULT_RANDOM_LENGTH + 1))
 
 
 def get_random_from_format_test2():  # sometimes not passing
     options = get_default_option()
-    for _ in range(10):
+    for _ in range(RANDOM_TEST_NUMBER):
         options.fo = "[int]"
         options._max = 0
         res = get_random_from_format(options)
@@ -32,58 +35,64 @@ def get_random_from_format_test2():  # sometimes not passing
 def get_random_from_format_test3():
     options = get_default_option()
     options.fo = "[int0]"
-    res = get_random_from_format(options)
-    assert(re.search(r'(-)?\d+', res))
-    res = int(res)
-    assert(res >= 0)
+    for _ in range(RANDOM_TEST_NUMBER):
+        res = get_random_from_format(options)
+        assert(re.search(r'(-)?\d+', res))
+        res = int(res)
+        assert(res >= 0)
 
 
 def get_random_from_format_test4():
     options = get_default_option()
     options.fo = "[int:100]"
-    res = get_random_from_format(options)
-    assert(re.search(r'(-)?\d+', res))
-    res = int(res)
-    assert(res <= 100)
+    for _ in range(RANDOM_TEST_NUMBER):
+        res = get_random_from_format(options)
+        assert(re.search(r'(-)?\d+', res))
+        res = int(res)
+        assert(res <= 100)
 
 
 def get_random_from_format_test5():
     options = get_default_option()
     options.fo = "[int0:100]"
-    res = get_random_from_format(options)
-    assert(re.search(r'(-)?\d+', res))
-    res = int(res)
-    assert(0 <= res <= 100)
+    for _ in range(RANDOM_TEST_NUMBER):
+        res = get_random_from_format(options)
+        assert(re.search(r'(-)?\d+', res))
+        res = int(res)
+        assert(0 <= res <= 100)
 
 
 def get_random_from_format_test6():
     options = get_default_option()
     options.fo = "[double0]"
-    res = get_random_from_format(options)
+    for _ in range(RANDOM_TEST_NUMBER):
+        res = get_random_from_format(options)
 
-    assert(re.search(r'[+-]?[0-9]*[\.]?[0-9]+', res))
-    res = float(res)
-    assert(res >= 0)
+        assert(re.search(r'[+-]?[0-9]*[\.]?[0-9]+', res))
+        res = float(res)
+        assert(res >= 0)
 
 
 def get_random_from_format_test6():
     options = get_default_option()
     options.fo = "[double:100]"
-    res = get_random_from_format(options)
+    for _ in range(RANDOM_TEST_NUMBER):
+        res = get_random_from_format(options)
 
-    assert(re.search(r'[+-]?[0-9]*[\.]?[0-9]+', res))
-    res = float(res)
-    assert(res <= 100)
+        assert(re.search(r'[+-]?[0-9]*[\.]?[0-9]+', res))
+        res = float(res)
+        assert(res <= 100)
 
 
 def get_random_from_format_test7():
     options = get_default_option()
     options.fo = "[double0:100]"
-    res = get_random_from_format(options)
+    for _ in range(RANDOM_TEST_NUMBER):
+        res = get_random_from_format(options)
 
-    assert(re.search(r'[+-]?[0-9]*[\.]?[0-9]+', res))
-    res = float(res)
-    assert(0 <= res <= 100)
+        assert(re.search(r'[+-]?[0-9]*[\.]?[0-9]+', res))
+        res = float(res)
+        assert(0 <= res <= 100)
 
 
 class RandomTypeTestCase(unittest.TestCase):
@@ -117,52 +126,61 @@ class IntRandomTestCase(unittest.TestCase):
 
     def get_random_test(self):
         r = IntRandomType()
-        assert(
-            -10**(DEFAULT_RANDOM_LENGTH + 1) < r.get_random() <
-            10**(DEFAULT_RANDOM_LENGTH + 1))
+        for _ in range(RANDOM_TEST_NUMBER):
+            assert(
+                -10**(DEFAULT_RANDOM_LENGTH + 1) < r.get_random() <
+                10**(DEFAULT_RANDOM_LENGTH + 1))
 
     def get_random_test2(self):
         r = IntRandomType(min_value=5 * 10**(DEFAULT_RANDOM_LENGTH - 1))
-        assert(5 * 10**(DEFAULT_RANDOM_LENGTH - 1) < r.get_random() <
-               10**DEFAULT_RANDOM_LENGTH)
+        for _ in range(RANDOM_TEST_NUMBER):
+            assert(5 * 10**(DEFAULT_RANDOM_LENGTH - 1) < r.get_random() <
+                   10**DEFAULT_RANDOM_LENGTH)
 
     def get_random_test3(self):
         r = IntRandomType(max_value=-5 * 10**(DEFAULT_RANDOM_LENGTH - 1))
-        assert(r.get_random() <= -5 * 10**(DEFAULT_RANDOM_LENGTH - 1))
+        for _ in range(RANDOM_TEST_NUMBER):
+            assert(r.get_random() <= -5 * 10**(DEFAULT_RANDOM_LENGTH - 1))
 
 
 class StrRandomTypeTestCase(unittest.TestCase):
 
     def get_random_test(self):
         r = StrRandomType()
-        assert(len(r.get_random()) == DEFAULT_RANDOM_LENGTH)
-        assert(r.get_random()[
-               0] in 'abcdefghiklmnopqrstuvwxyzABCDEFGHIKLMNOPQRSTUVWXYZ_')
-        assert(r.get_random()[1] in
+        for _ in range(RANDOM_TEST_NUMBER):
+            assert(len(r.get_random()) == DEFAULT_RANDOM_LENGTH)
+            assert(r.get_random()[0]
+               in 'abcdefghiklmnopqrstuvwxyzABCDEFGHIKLMNOPQRSTUVWXYZ_')
+            assert(r.get_random()[1] in
                'abcdefghiklmnopqrstuvwxyzABCDEFGHIKLMNOPQRSTUVWXYZ_123456789')
 
     def get_random_test2(self):
-        assert(len(StrRandomType(length=100).get_random()) == 100)
+        for _ in range(RANDOM_TEST_NUMBER):
+            assert(len(StrRandomType(length=100).get_random()) == 100)
 
     def get_random_test3(self):
-        assert(StrRandomType(length=0).get_random() == '')
+        for _ in range(RANDOM_TEST_NUMBER):
+            assert(StrRandomType(length=0).get_random() == '')
 
 
 class DoubleRandomTypeTestCase(unittest.TestCase):
 
     def get_random_test(self):
         r = DoubleRandomType()
-        assert(
-            -10**(DEFAULT_RANDOM_LENGTH + 1) < r.get_random() <
-            10**(DEFAULT_RANDOM_LENGTH + 1))
+        for _ in range(RANDOM_TEST_NUMBER):
+            assert(
+                -10**(DEFAULT_RANDOM_LENGTH + 1) < r.get_random() <
+                10**(DEFAULT_RANDOM_LENGTH + 1))
 
     def get_random_test2(self):
         r = DoubleRandomType(max_value=0)
-        assert(r.get_random() <= 0)
+        for _ in range(RANDOM_TEST_NUMBER):
+            assert(r.get_random() <= 0)
 
     def get_random_test3(self):
         r = DoubleRandomType(min_value=0)
-        assert(r.get_random() >= 0)
+        for _ in range(RANDOM_TEST_NUMBER):
+            assert(r.get_random() >= 0)
 
 
 def get_random_type_from_options_test():
